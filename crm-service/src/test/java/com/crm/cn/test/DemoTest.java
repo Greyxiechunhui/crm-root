7e27b7056dd0b2b2d43da58be528546dfb85b9fa
chunhui <greyxiechunhui@163.com>
2020-10-16 22:55:46 +0800

    init

---
+++ crm-service/src/test/java/com/crm/cn/test/DemoTest.java
@@ -0,0 +1,28 @@
+package com.crm.cn.test;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@ContextConfiguration(locations = {"classpath*:applicationContext-mapper.xml","classpath:applicationContext-service.xml"})
+//public class DemoTest {
+//    @Autowired
+//    private AdminMapper adminMapper;
+//    @Test
+//    public void doTest(){
+////        System.out.println(adminMapper.selectList(null));
+////        Admin admin = adminMapper.selectById(1);
+////        admin.setId(10);
+////        admin.setPhone("");
+////        admin.setEmail("");
+////        adminMapper.insert(admin);
+//        QueryWrapper<Admin> wrapper = new QueryWrapper<Admin>();
+//        wrapper.eq("id",10);
+//        adminMapper.delete(wrapper);
+//    }
+//
+//}
